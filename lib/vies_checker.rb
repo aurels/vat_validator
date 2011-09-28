ac6996925df3f8a2e0aa91e13910338f922fb53f
Francois Stephany <tulipe.moutarde@gmail.com>
2011-09-28 15:03:58 +0200

    syntaxic simplification

--- lib/vies_checker.rb
+++ lib/vies_checker.rb
@@ -2,28 +2,25 @@ require 'rubygems'
 require 'savon'
 
 module VatValidator
-  class ViesChecker
-    
-    class << self
-      def check(country_code, vat_number, vies_host='http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl')
-        client = Savon::Client.new(vies_host)
+  class ViesChecker  
+    def self.check(country_code, vat_number, vies_host='http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl')
+      client = Savon::Client.new(vies_host)
 
-        begin
-          response = client.request :check_vat do
-            soap.body = {
-              :country_code => country_code, 
-              :vat_number => vat_number
-            }
-          end
-        rescue
-          raise ViesContactError
+      begin
+        response = client.request :check_vat do
+          soap.body = {
+            :country_code => country_code, 
+            :vat_number => vat_number
+          }
         end
+      rescue
+        raise ViesContactError
+      end
 
-        if response.success?
-          return response[:check_vat_response][:valid]
-        else
-          raise ViesContactError
-        end
+      if response.success?
+        return response[:check_vat_response][:valid]
+      else
+        raise ViesContactError
       end
     end
   end
